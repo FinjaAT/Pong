c7c494d3e584d32ce40bc7a089b65f763e5403a7
FinjaAT <finja.wede@gmail.com>
2026-04-30 10:15:58 +0200

    modified input data to use 10 time states instead of one

--- mine.py
+++ mine.py
@@ -1,22 +1,26 @@
+from collections import deque
+import numpy as np
 import pong_rl_environment as envi
 import deepQ_agent as ag
 import random
 
+history_len = 10
 env = envi.pong_environment(render=True)
-agent = ag.my_agent(8,3)
+agent = ag.my_agent(8 * history_len, 3)
 
 positiondata = env.give_start_state()
+history = deque([positiondata] * history_len, maxlen=history_len)
 
 while True:
-    
-    actionrightpaddle = agent.get_action(positiondata)
+    state = np.concatenate(history)
+    actionrightpaddle = agent.get_action(state)
 
-    state = positiondata
+    next_positiondata, reward, rewardleft, done, running = env.one_step(actionrightpaddle)
 
-    positiondata, reward,rewardleft,done,running = env.one_step(actionrightpaddle)
+    history.append(next_positiondata)
+    next_state = np.concatenate(history)
 
-    agent.memory.append((state, actionrightpaddle, reward, positiondata, done))
-
+    agent.memory.append((state, actionrightpaddle, reward, next_state, done))
     agent.train()
 
     if not running:
